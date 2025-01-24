a0c62b58c7b67b5dfac749fd5f902f7ba089bc13
melih <melih.g.asd@gmail.com>
2025-01-24 03:43:08 +0300

    .

--- TourFlowManager/CustomerPage/CustomerMainPage.Designer.cs
+++ TourFlowManager/CustomerPage/CustomerMainPage.Designer.cs
@@ -38,9 +38,9 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.Sienna;
-            this.label1.Location = new System.Drawing.Point(212, 104);
+            this.label1.Location = new System.Drawing.Point(211, 102);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(451, 39);
+            this.label1.Size = new System.Drawing.Size(436, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "Müşteri Sayfası Çok Yakında";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
--- TourFlowManager/TourGuidePage/TourGuideMainPage.Designer.cs
+++ TourFlowManager/TourGuidePage/TourGuideMainPage.Designer.cs
@@ -38,9 +38,9 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.Sienna;
-            this.label1.Location = new System.Drawing.Point(202, 94);
+            this.label1.Location = new System.Drawing.Point(206, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(520, 39);
+            this.label1.Size = new System.Drawing.Size(501, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tur Rehberi Sayfası Çok Yakında";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
